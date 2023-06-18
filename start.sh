d815065bd6c398a310bb86302632b17baa1900fc
mikutechnology <2444936371@qq.com>
2023-06-19 00:00:43 +0800

    添加prowlarr索引

--- start.sh
+++
@@ -1,4 +0,0 @@
-cd /volume1/data/nas-tools-jackett
-export NASTOOL_CONFIG="/volume1/data/nas-tools-jackett/config/config.yaml"
-python3.10 -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-python3.10 run.py &
